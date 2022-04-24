8af4eace726692b50b42748fa8daafd5096db0a0
FabTrindade <fabrtrindade@gmail.com>
2022-04-24 20:17:26 -0300

    Add DataAnotations to control input information

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +10,12 @@ namespace MovieAPI.Models
 {
     public class Movie
     {
+        [Required (ErrorMessage = "The Title field is required.")]
         public string Title { get; set; }
+        [Required (ErrorMessage = "The Diector field is required.")]
         public string Director { get; set; }
         public string Genre { get; set; }        
+        [Range(1, 360)]
         public int Duration { get; set; }        
         
     }
