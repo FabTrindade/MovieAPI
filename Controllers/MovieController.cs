55dd7fdb3ef1763b220f7625c4a61841b5e8a34f
FabTrindade <fabrtrindade@gmail.com>
2022-04-24 19:00:45 -0300

    Project created!

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MovieAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+    }
+}
