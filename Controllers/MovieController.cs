1968e3f698357009cbd1a8093b439c160431df89
FabTrindade <fabrtrindade@gmail.com>
2022-04-26 19:36:08 -0300

    First use of DB context in MovieController for POST and GET methods

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MovieAPI.Data;
 using MovieAPI.Models;
 
 namespace MovieAPI.Controllers
@@ -12,19 +13,33 @@ namespace MovieAPI.Controllers
     [Route("[controller]")]
     public class MovieController : ControllerBase
     {
-        private static List<Movie> movies = new List<Movie>();
+        private MoviesContext _contex;
+
+        public MovieController(MoviesContext contex)
+        {
+            _contex = contex;
+        }
 
         [HttpPost]
-        public void AddMovie([FromBody] Movie movie)
+        public IActionResult AddMovie([FromBody] Movie movie)
         {
-            movies.Add(movie);
-            Console.WriteLine(movie.Title);
+            _contex.Movies.Add(movie);
+            _contex.SaveChanges();
+            return CreatedAtAction(nameof(GetMoviesById), new {Id = movie.Id}, movie);
         }
         
         [HttpGet]
         public IEnumerable<Movie> GetMovies()
         {
-            return movies;
+            return (_contex.Movies);
         }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetMoviesById(int id)
+        {
+            Movie movie = _contex.Movies.FirstOrDefault(movie => movie.Id == id);            
+            return (movie != null)? Ok(movie) : NotFound();
+        }
+
     }
 }
